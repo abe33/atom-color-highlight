fda99a131c5f2510f07070c58588c545c4140d30
abe33 <cedric.nehemie@gmail.com>
2014-03-14 21:58:46 +0100

    chore($git): changes change log script repository path

--- changelog.js
+++ changelog.js
@@ -12,8 +12,8 @@ var GIT_LOG_CMD = 'git log --grep="%s" -E --format=%s %s..HEAD';
 var GIT_TAG_CMD = 'git describe --tags --abbrev=0';
 
 var HEADER_TPL = '<a name="%s"></a>\n# %s (%s)\n\n';
-var LINK_ISSUE = '[#%s](https://github.com/angular/angular.js/issues/%s)';
-var LINK_COMMIT = '[%s](https://github.com/angular/angular.js/commit/%s)';
+var LINK_ISSUE = '[#%s](https://github.com/abe33/atom-color-highlight/issues/%s)';
+var LINK_COMMIT = '[%s](https://github.com/abe33/atom-color-highlight/commit/%s)';
 
 var EMPTY_COMPONENT = '$$';
 
